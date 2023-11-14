10f796c67213dca55441745c0e044985b5e6a38a
Lukas Grimm <lukaskingsmail@gmail.com>
2023-11-14 21:32:06 +0100

    Started implementing gRPC

--- server.go
+++ server.go
@@ -1,20 +1,73 @@
 package queueing
 
-import "context"
+import (
+	"context"
+	"crypto/sha256"
+	"github.com/google/uuid"
+	"time"
+)
 
 type WriteAheadLog interface {
-	Write([]byte) (int, error)
+	Write(ctx context.Context, message *QueueMessage) error
 }
 
 type MessageQueueingServer struct {
 	UnimplementedQueueServiceServer
+
+	wal WriteAheadLog
 }
 
 func (m MessageQueueingServer) SubmitMessages(
 	ctx context.Context, request *SubmitMessagesRequest,
 ) (*SubmitMessagesResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	receipts := make([]*SubmitReceipt, len(request.Messages))
+
+	for _, rawQueueMessage := range request.Messages {
+		queueMessage := rawQueueMessage.ToQueueMessage()
+		err := m.wal.Write(ctx, queueMessage)
+		if err != nil {
+			f := false
+			reason := err.Error()
+
+			receipts = append(
+				receipts, &SubmitReceipt{
+					Ok:        &f,
+					MessageID: queueMessage.MessageID,
+					Reason:    &reason,
+				},
+			)
+		} else {
+			t := true
+
+			receipts = append(
+				receipts, &SubmitReceipt{
+					Ok:        &t,
+					MessageID: queueMessage.MessageID,
+					Timestamp: queueMessage.Timestamp,
+					DataHash:  queueMessage.DataHash,
+				},
+			)
+		}
+	}
+
+	return &SubmitMessagesResponse{
+		Receipts: receipts,
+	}, nil
+}
+
+func (m *RawQueueMessage) ToQueueMessage() *QueueMessage {
+	messageID := uuid.NewString()
+	now := time.Now().Unix()
+	hash := sha256.New()
+	hash.Write(m.RawMessage)
+
+	return &QueueMessage{
+		MessageID:  &messageID,
+		Timestamp:  &now,
+		Attributes: m.Attributes,
+		Data:       m.RawMessage,
+		DataHash:   hash.Sum(nil),
+	}
 }
 
 func (m MessageQueueingServer) RetrieveMessages(
