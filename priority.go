1f51f1affc0b5ec4b042dcdb1cc0cc4aced7e0a8
Lukas Grimm <lukaskingsmail@gmail.com>
2023-11-23 18:44:03 +0100

    Basic implementation of a write ahead log

--- priority.go
+++ priority.go
@@ -2,9 +2,15 @@ package queueing
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/google/uuid"
 	"io"
+)
+
+var (
+	WalError                    = errors.New("error writing to write ahead log")
+	FatalDequeueMitigationError = errors.New("fatal error! writing to write ahead log failed AND could not enqueue messages again! potential data loss")
 )
 
 type Queue[Value any] interface {
@@ -21,6 +27,11 @@ type Repository interface {
 	Delete(message *QueueMessage) error
 }
 
+type walEvent struct {
+	EventType string `json:"type"`
+	Data      any    `json:"data"`
+}
+
 type walQueueService struct {
 	log io.Writer
 
@@ -35,7 +46,15 @@ func NewWriteAheadLogQueueService(writer io.Writer, service Queue[*QueueMessage]
 }
 
 func (queue *walQueueService) Enqueue(ctx context.Context, messages ...*QueueMessage) error {
-	queue.log.Write([]byte{})
+	err := json.NewEncoder(queue.log).Encode(
+		walEvent{
+			EventType: "enqueue",
+			Data:      messages,
+		},
+	)
+	if err != nil {
+		return err
+	}
 
 	return queue.service.Enqueue(ctx, messages...)
 }
@@ -43,7 +62,20 @@ func (queue *walQueueService) Enqueue(ctx context.Context, messages ...*QueueMes
 func (queue *walQueueService) Dequeue(ctx context.Context, messages []*QueueMessage) (int, error) {
 	n, err := queue.service.Dequeue(ctx, messages)
 
-	queue.log.Write([]byte{})
+	walErr := json.NewEncoder(queue.log).Encode(
+		walEvent{
+			EventType: "dequeue",
+			Data:      messages,
+		},
+	)
+
+	if walErr != nil {
+		enqErr := queue.service.Enqueue(ctx, messages...)
+		if enqErr != nil {
+			return 0, errors.Join(walErr, enqErr, WalError, FatalDequeueMitigationError)
+		}
+		return 0, errors.Join(WalError, FatalDequeueMitigationError)
+	}
 
 	return n, err
 }
