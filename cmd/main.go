ee371a2207f6805aabf67c85ac7f8142e95b496b
Lukas Grimm <lukaskingsmail@gmail.com>
2023-11-24 17:38:07 +0100

    Implement otel for main service

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	api "go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"google.golang.org/grpc"
 	"log"
 	queueing "message-queueing"
+	"message-queueing/otel"
 	"net"
 	"os"
 	"time"
@@ -14,6 +22,8 @@ import (
 var port = flag.Int("port", 8080, "the port of the application")
 
 func main() {
+	setupOTel()
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 
@@ -33,9 +43,50 @@ func main() {
 
 	service := queueing.NewQueueService(repo, queueing.NewTimeoutQueue())
 	service = queueing.NewWriteAheadLogQueueService(walFile, service)
+	service, err = otel.WrapOTelPriorityQueue(service)
+	if err != nil {
+		panic(err)
+	}
 
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	queueing.RegisterQueueServiceServer(grpcServer, queueing.NewMessageQueueingServer(service))
 	grpcServer.Serve(lis)
+}
+
+func setupOTel() {
+	traceExporter, err := stdouttrace.New(
+		stdouttrace.WithPrettyPrint(), stdouttrace.WithWriter(log.Writer()), stdouttrace.WithoutTimestamps(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("ExampleService"),
+		),
+	)
+
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(traceExporter),
+		sdktrace.WithResource(r),
+	)
+
+	meterExporter, err := stdoutmetric.New(
+		stdoutmetric.WithoutTimestamps(), stdoutmetric.WithPrettyPrint(), stdoutmetric.WithWriter(log.Writer()),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(meterExporter, sdkmetric.WithInterval(3*time.Second))),
+		sdkmetric.WithResource(r),
+	)
+
+	api.SetTracerProvider(tracerProvider)
+	api.SetMeterProvider(meterProvider)
 }
---
+++ otel/priority_wrapper.go
@@ -0,0 +1,90 @@
+package otel
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
+	queueing "message-queueing"
+)
+
+type otelPriorityQueue struct {
+	messagesWritten      metric.Int64Counter
+	messagesRetrieved    metric.Int64Counter
+	messagesAcknowledged metric.Int64Counter
+	tracer               trace.Tracer
+
+	service queueing.Queue[*queueing.QueueMessage]
+}
+
+func WrapOTelPriorityQueue(service queueing.Queue[*queueing.QueueMessage]) (
+	queueing.Queue[*queueing.QueueMessage], error,
+) {
+	meter := otel.Meter("message-queueing")
+
+	messagesWritten, err := meter.Int64Counter("messagesWritten")
+	if err != nil {
+		return nil, err
+	}
+
+	messagesRetrieved, err := meter.Int64Counter("messageRetrieved")
+	if err != nil {
+		return nil, err
+	}
+
+	messagesAcknowledged, err := meter.Int64Counter("messagesAcknowledged")
+	if err != nil {
+		return nil, err
+	}
+
+	return &otelPriorityQueue{
+		service: service,
+
+		messagesWritten:      messagesWritten,
+		messagesRetrieved:    messagesRetrieved,
+		messagesAcknowledged: messagesAcknowledged,
+
+		tracer: otel.Tracer("message-queueing"),
+	}, nil
+}
+
+func (o *otelPriorityQueue) Enqueue(ctx context.Context, messages ...*queueing.QueueMessage) error {
+	ctx, span := o.tracer.Start(ctx, "Enqueue")
+	defer span.End()
+
+	o.messagesWritten.Add(ctx, int64(len(messages)))
+
+	return o.service.Enqueue(ctx, messages...)
+}
+
+func (o *otelPriorityQueue) Dequeue(ctx context.Context, messages []*queueing.QueueMessage) (int, error) {
+	ctx, span := o.tracer.Start(ctx, "Dequeue")
+	defer span.End()
+
+	n, err := o.service.Dequeue(ctx, messages)
+
+	return n, err
+}
+
+func (o *otelPriorityQueue) Retrieve(ctx context.Context, messages []*queueing.QueueMessage) (int, error) {
+	ctx, span := o.tracer.Start(ctx, "Retrieve")
+	defer span.End()
+
+	n, err := o.service.Retrieve(ctx, messages)
+
+	o.messagesRetrieved.Add(ctx, int64(n))
+
+	return n, err
+}
+
+func (o *otelPriorityQueue) Acknowledge(ctx context.Context, messageID uuid.UUID) error {
+	ctx, span := o.tracer.Start(ctx, "Acknowledge")
+	defer span.End()
+
+	err := o.service.Acknowledge(ctx, messageID)
+
+	o.messagesAcknowledged.Add(ctx, 1)
+
+	return err
+}
