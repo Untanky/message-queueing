213c965595edbfd74e2898815ee4f3aa0d1e8fed
Lukas Grimm <lukaskingsmail@gmail.com>
2023-11-25 09:08:39 +0100

    Add latency metric to the server

--- otel/server_otel.go
+++ otel/server_otel.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"go.opentelemetry.io/otel/metric"
 	queueing "message-queueing"
+	"time"
 )
 
 type otelQueueServiceServer struct {
 	numberOfRequests metric.Int64Counter
-	numberOfError    metric.Int64Counter
+	numberOfErrors   metric.Int64Counter
+	latency          metric.Int64Histogram
 
 	server queueing.QueueServiceServer
 
@@ -21,15 +23,29 @@ func WrapQueueServiceServer(server queueing.QueueServiceServer) (queueing.QueueS
 		return nil, err
 	}
 
+	numberOfErrors, err := meter.Int64Counter("numberOfErrors")
+	if err != nil {
+		return nil, err
+	}
+
+	latency, err := meter.Int64Histogram("requestLatency", metric.WithUnit("μs"))
+	if err != nil {
+		return nil, err
+	}
+
 	return otelQueueServiceServer{
 		numberOfRequests: numberOfRequests,
-		server:           server,
+		numberOfErrors:   numberOfErrors,
+		latency:          latency,
+
+		server: server,
 	}, nil
 }
 
 func (o otelQueueServiceServer) SubmitMessages(
 	ctx context.Context, request *queueing.SubmitMessagesRequest,
 ) (*queueing.SubmitMessagesResponse, error) {
+	start := time.Now()
 	ctx, span := tracer.Start(ctx, "SubmitMessages")
 	defer span.End()
 
@@ -37,9 +53,12 @@ func (o otelQueueServiceServer) SubmitMessages(
 
 	response, err := o.server.SubmitMessages(ctx, request)
 	if err != nil {
-		o.numberOfError.Add(ctx, 1)
+		o.numberOfErrors.Add(ctx, 1)
 		span.RecordError(err)
 	}
+
+	end := time.Now()
+	o.latency.Record(ctx, end.Sub(start).Microseconds())
 
 	return response, err
 }
@@ -47,6 +66,7 @@ func (o otelQueueServiceServer) SubmitMessages(
 func (o otelQueueServiceServer) RetrieveMessages(
 	ctx context.Context, request *queueing.RetrieveMessagesRequest,
 ) (*queueing.RetrieveMessagesResponse, error) {
+	start := time.Now()
 	ctx, span := tracer.Start(ctx, "RetrieveMessages")
 	defer span.End()
 
@@ -54,9 +74,12 @@ func (o otelQueueServiceServer) RetrieveMessages(
 
 	response, err := o.server.RetrieveMessages(ctx, request)
 	if err != nil {
-		o.numberOfError.Add(ctx, 1)
+		o.numberOfErrors.Add(ctx, 1)
 		span.RecordError(err)
 	}
+
+	end := time.Now()
+	o.latency.Record(ctx, end.Sub(start).Microseconds())
 
 	return response, err
 }
@@ -64,6 +87,7 @@ func (o otelQueueServiceServer) RetrieveMessages(
 func (o otelQueueServiceServer) AcknowledgeMessages(
 	ctx context.Context, request *queueing.AcknowledgeMessagesRequest,
 ) (*queueing.AcknowledgeMessagesResponse, error) {
+	start := time.Now()
 	ctx, span := tracer.Start(ctx, "AcknowledgeMessages")
 	defer span.End()
 
@@ -71,9 +95,12 @@ func (o otelQueueServiceServer) AcknowledgeMessages(
 
 	response, err := o.server.AcknowledgeMessages(ctx, request)
 	if err != nil {
-		o.numberOfError.Add(ctx, 1)
+		o.numberOfErrors.Add(ctx, 1)
 		span.RecordError(err)
 	}
+
+	end := time.Now()
+	o.latency.Record(ctx, end.Sub(start).Microseconds())
 
 	return response, err
 }
