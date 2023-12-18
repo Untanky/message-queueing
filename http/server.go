8a3ef1ee934c2619d82bf7df0138920bd62eaa93
Lukas Grimm <lukaskingsmail@gmail.com>
2023-12-18 15:29:06 +0100

    Implement custom logger

--- http/server.go
+++ http/server.go
@@ -1,22 +1,19 @@
 package http
 
 import (
-	"crypto/tls"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"log/slog"
 	queueing "message-queueing"
 	"net/http"
+	"time"
 )
 
 func NewServer(service queueing.Service, storage queueing.BlockStorage, repository queueing.Repository) http.Handler {
-	router := gin.Default()
-
-	tls.NewListener(
-		nil, &tls.Config{
-			ClientAuth: tls.RequireAndVerifyClientCert,
-		},
-	)
+	router := gin.New()
 
 	router.Use(gin.Recovery())
+	router.Use(logger())
 
 	controller := QueueMessageController{
 		service: service,
@@ -41,4 +38,38 @@ func NewServer(service queueing.Service, storage queueing.BlockStorage, reposito
 	internal.PUT("/queues/:queueID/messages/:messageID", internalCtrlr.updateMessage)
 
 	return router
+}
+
+func logger() gin.HandlerFunc {
+	log := slog.Default().With("service", "http-server")
+
+	return func(ctx *gin.Context) {
+		start := time.Now()
+
+		requestID := ctx.Request.Header.Get("request-id")
+		if requestID == "" {
+			requestID = uuid.NewString()
+		}
+
+		ctx.Set("request-id", requestID)
+		ctx.Header("request-id", requestID)
+
+		ctx.Next()
+
+		duration := time.Now().Sub(start)
+
+		attr := []slog.Attr{
+			slog.Int("status", ctx.Writer.Status()),
+			slog.String("method", ctx.Request.Method),
+			slog.String("path", ctx.Request.URL.Path),
+			slog.Duration("latency", duration),
+			slog.String("host", ctx.ClientIP()),
+			slog.Int("bytes", ctx.Writer.Size()),
+		}
+		if ctx.Writer.Status() < 400 {
+			log.InfoContext(ctx, "request succeeded", "request", attr, "requestID", requestID)
+		} else {
+			log.ErrorContext(ctx, "request failed", "request", attr, "requestID", requestID)
+		}
+	}
 }
